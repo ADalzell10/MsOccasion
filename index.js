e7768970e19b646a19f65bf368a3abfce1899052
Aaron Dalzell <aarondalzell16@gmail.com>
2021-04-21 16:40:29 +0100

    some server side validation applied using joi

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const ejsMate = require("ejs-mate");
+const { productSchema } = require("./schemas.js");
+// wraps async function in try catch
+const catchAsync = require('./utils/catchAsync');
+// catchs errors and reports a message
+const ExpressError = require('./utils/ExpressError');
 const path = require("path");
 const mongoose = require("mongoose");
+// allows product info to be deleted by overriding method
 const methodOverride = require("method-override")
 
+// mongoose product mondel
 const Product = require('./models/product');
+const { statSync } = require("fs");
 
 // connecting db
 mongoose.connect('mongodb://localhost:27017/msoccasion', {
    useNewUrlParser: true,
    useCreateIndex: true,
-   useUnifiedTopology: true
+   useUnifiedTopology: true,
+   useFindAndModify: false
 });
 
+// displays message to verify the db is connected or if error
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
@@ -32,6 +42,17 @@ app.use(methodOverride("_method"));
 // public directory includes js and css files
 app.use(express.static(__dirname + '/public'));
 
+// server side validation using Joi
+const validateProduct = function (req, res, next) {
+   const { error } = productSchema.validate(req.body);
+   if (error) {
+      const msg = error.details.map(el => el.message).join(",")
+      throw new ExpressError(msg, 400);
+   } else {
+      next();
+   }
+}
+
 // home/welcome page for user
 app.get("/home", function (req, res) {
    res.render("home");
@@ -42,10 +63,10 @@ app.get("/", function (req, res) {
 });
 
 // product display page
-app.get("/products", async function (req, res) {
+app.get("/products", catchAsync(async function (req, res) {
    const products = await Product.find({});
    res.render("products/index", { products });
-});
+}));
 
 // Renders form to add a new product to site
 app.get("/products/new", function (req, res) {
@@ -58,41 +79,47 @@ app.get("/enquire", function (req, res) {
 });
 
 // Saves a newly created product to the db and displays on index
-app.post("/products", async function (req, res) {
+app.post("/products", validateProduct, catchAsync(async function (req, res) {
    const product = new Product(req.body.product);
    await product.save();
    res.redirect(`/products/${product._id}`);
-})
+}))
 
 // Displays show page for a product
-app.get("/products/:id", async function (req, res) {
+app.get("/products/:id", catchAsync(async function (req, res) {
    const product = await Product.findById(req.params.id);
    res.render("products/showProduct", { product });
-});
+}));
 
 // Renders the edit form for a current product
-app.get("/products/:id/edit", async function (req, res) {
+app.get("/products/:id/edit", catchAsync(async function (req, res) {
    const product = await Product.findById(req.params.id);
    res.render("products/editProduct", { product });
-});
+}));
 
 // Send a put request to update the information received from the update form
-app.put("/products/:id", async function (req, res) {
+app.put("/products/:id", validateProduct, catchAsync(async function (req, res) {
    const { id } = req.params;
    const product = await Product.findByIdAndUpdate(id, { ...req.body.product });
    res.redirect(`/products/${product._id}`);
-})
+}));
 
 // Will look up the product by id and delete from DB
-app.delete("/products/:id", async function (req, res) {
+app.delete("/products/:id", catchAsync(async function (req, res) {
    const { id } = req.params;
    await Product.findByIdAndDelete(id);
    res.redirect("/products");
-});
+}));
 
 // root to handle pages which do not exist
-app.get("*", function (req, res) {
-   res.send("404 Page not found");
+app.all("*", function (req, res, next) {
+   next(new ExpressError("Page not found", 404));
+})
+
+app.use(function (err, req, res, next) {
+   const { statusCode = 500 } = err;
+   if (!err.message) err.message = "Something went wrong";
+   res.status(statusCode).render("error", { err });
 })
 
 app.listen(3000, function () {
